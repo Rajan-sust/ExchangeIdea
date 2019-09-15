bb936374c10e19956e5d41c3ebb5edd362422c0e
Rajan-sust <imrajon0@gmail.com>
2019-09-15 14:17:49 +0600

    adding cookies

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
 var crypto = require('crypto');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 var url = "mongodb://127.0.0.1:27017";
 
@@ -31,17 +33,16 @@ router.post('/register', function (req, res, next) {
   var email = req.body.email;
   var password = req.body.password;
 
-  MongoClient.connect(url, function(err, client) {
+  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
     if (err) throw err;
     var myobj = {
       username : username,
       email: email,
       password: getHash(password)
     };
-    var db = client.db("exchange-idea");
-    db.collection("users").insertOne(myobj, function(err, res) {
+    const collection = client.db("exchange-idea").collection("users");
+    collection.insertOne(myobj, function(err, res) {
       if (err) throw err;
-      console.log("Document of 1 User Inserted");
       client.close();
     });
   });
@@ -53,6 +54,40 @@ router.post('/register', function (req, res, next) {
 
 router.get('/login', function(req, res, next) {
   res.render('login');
+
+});
+
+
+router.post('/login', function(req, res, next) {
+
+  console.log('After posting login form');
+
+  var email = req.body.email;
+  var password = getHash(req.body.password);
+ 
+
+  MongoClient.connect(url, { useNewUrlParser: true , useUnifiedTopology: true}, function(err, client) {
+    if (err) throw err;
+    var query = {
+      email: email,
+      password: password
+    };
+    const collection = client.db("exchange-idea").collection("users");
+    collection.find(query).toArray(function(err, items) {
+      if(items.length == 1) {
+        res.cookie('userData',items[0],{ maxAge: 1000 * 60 * 60 * 24 });
+        res.redirect('/home');
+      } else{
+        res.redirect('/login?error=' + encodeURIComponent('incorrect'));
+      }
+      client.close();
+    });
+  });
+
+ 
+
+
+
 
 });
 
@@ -69,5 +104,6 @@ module.exports = router;
 show dbs
 use mydb
 show collections
+db.collection_name.find().pretty()
 */
 
